dc7a82087f07c0258b76bbf51c3004d4b397af6b
asadcse100 <asadcse100@gmail.com>
2024-03-21 01:44:14 +0600

    first commit

---
+++ api/v2/endpoints/most_watched.php
@@ -0,0 +1,12 @@
+<?php
+$response_data = array(
+    'api_status' => 400
+);
+
+$limit = (!empty($_POST['limit']) && is_numeric($_POST['limit']) && $_POST['limit'] > 0 && $_POST['limit'] <= 50 ? Wo_Secure($_POST['limit']) : 20);
+
+$most_matched = Wo_GetMtwFilms($limit);
+$response_data = array(
+    'api_status' => 200,
+    'data'         => $most_matched
+);
